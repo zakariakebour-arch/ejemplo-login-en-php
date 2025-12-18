24392636ec00cc1bdbb4b4b5b03f9280fbef1b67
Zakaria <zakariakebour@gmail.com>
2025-12-18 10:12:12 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,10 @@
-
+<?php
+  fuction conexion(){
+      $host = "localhost";
+      $user = "pablo";
+      $pass = "1234";
+      $db = "prueba_php";
+      $conexion = new mysqli($host,$user,$pass,$db);
+      return $conexion;
+    }
+?>
